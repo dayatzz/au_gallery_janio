cd411ee681d1edecc642be0a22725ba66ad37f44
dayatz <dayat.py@gmail.com>
2018-12-16 14:49:27 +0800

    group shipper_order_id by consignee and item name

--- au_gallery_janio.py
+++ au_gallery_janio.py
@@ -2,14 +2,13 @@ import xlrd
 import datetime
 import csv
 import sys
-import collections
 
 
 try:
     filename = sys.argv[1]
 except Exception:
     print(
-"""
+        """
   $ python au_galler_janio.py filename.xlsx
 """)
     exit()
@@ -54,64 +53,85 @@ sheet2_data = convert_to_dict(ws2, {
 })
 postals = {int(d['OrderId']): d['Zip Code'] for d in sheet2_data}
 
-janio_data = []
+
+def find_duplicate(obj, data):
+    filtered = filter(
+        lambda x: x['consignee_name'].lower() == obj['Customer Name'].lower()
+        and x['consignee_number'] == obj['Phone']
+        and x['consignee_address'].lower() == obj['Address'].lower()
+        and x['item_desc'].lower() == obj['SKU Name'].lower(),
+        data)
+    try:
+        return list(filtered)
+    except:
+        return []
+
+
+fields = [
+    'shipper_order_id',
+    'tracking_no',
+    'item_desc',
+    'item_quantity',
+    'item_product_id',
+    'item_sku',
+    'item_category',
+    'item_price_value',
+    'item_price_currency',
+    'consignee_name',
+    'consignee_number',
+    'consignee_address',
+    'consignee_postal',
+    'consignee_country',
+    'consignee_state',
+    'consignee_city',
+    'consignee_province',
+    'consignee_email',
+    'order_length',
+    'order_width',
+    'order_height',
+    'order_weight',
+    'payment_type',
+    'cod_amt_to_collect'
+]
+csv_data = []
 for d in sheet1_data:
-    obj = {
-        'shipper_order_id': int(d.get('OrderId', 0)),
-        'tracking_no': '',
-        'item_desc': d.get('SKU Name', ''),
-        'item_quantity': int(d.get('Quantity', 0)),
-        'item_product_id': '',
-        'item_sku': int(d.get('SKU Number', 0)),
-        'item_category': 'Lifestyle Products',
-        'item_price_value': d.get('PaySubtotal', ''),
-        'item_price_currency': 'IDR',
-        'consignee_name': d.get('Customer Name', ''),
-        'consignee_number': d.get('Phone', ''),
-        'consignee_address': d.get('Address', ''),
-        'consignee_postal': postals.get(d.get('OrderId', ''), ''),
-        'consignee_country': 'Indonesia',
-        'consignee_state': d.get('District', ''),
-        'consignee_city': d.get('City', ''),
-        'consignee_province': d.get('Province', ''),
-        'consignee_email': '',
-        'order_length': '',
-        'order_width': '',
-        'order_height': '',
-        'order_weight': '',
-        'payment_type': 'prepaid',
-        'cod_amt_to_collect': '',
-    }
-    janio_data.append(obj)
+    f = find_duplicate(d, csv_data)
+    if f:
+        index = csv_data.index(f[0])
+        csv_data[index]['shipper_order_id'] += ', {}'.format(
+            str(int(d['OrderId'])))
+        csv_data[index]['item_quantity'] += 1
+    else:
+        obj = {
+            'shipper_order_id': str(int(d.get('OrderId', 0))),
+            'tracking_no': '',
+            'item_desc': d.get('SKU Name', ''),
+            'item_quantity': int(d.get('Quantity', 0)),
+            'item_product_id': '',
+            'item_sku': int(d.get('SKU Number', 0)),
+            'item_category': 'Lifestyle Products',
+            'item_price_value': d.get('PaySubtotal', ''),
+            'item_price_currency': 'IDR',
+            'consignee_name': d.get('Customer Name', ''),
+            'consignee_number': d.get('Phone', ''),
+            'consignee_address': d.get('Address', ''),
+            'consignee_postal': postals.get(d.get('OrderId', ''), ''),
+            'consignee_country': 'Indonesia',
+            'consignee_state': d.get('District', ''),
+            'consignee_city': d.get('City', ''),
+            'consignee_province': d.get('Province', ''),
+            'consignee_email': '',
+            'order_length': 50,
+            'order_width': 50,
+            'order_height': 2,
+            'order_weight': 1,
+            'payment_type': 'prepaid',
+            'cod_amt_to_collect': '',
+        }
+        csv_data.append(obj)
 
 
 with open('janio_excel_csv.csv', 'w') as f:
-    fields = [
-        'shipper_order_id',
-        'tracking_no',
-        'item_desc',
-        'item_quantity',
-        'item_product_id',
-        'item_sku',
-        'item_category',
-        'item_price_value',
-        'item_price_currency',
-        'consignee_name',
-        'consignee_number',
-        'consignee_address',
-        'consignee_postal',
-        'consignee_country',
-        'consignee_state',
-        'consignee_city',
-        'consignee_province',
-        'consignee_email',
-        'order_length',
-        'order_width',
-        'order_height',
-        'order_weight',
-        'payment_type',
-        'cod_amt_to_collect'
-    ]
     w = csv.DictWriter(f, fieldnames=fields)
     w.writeheader()
-    w.writerows(janio_data)
+    w.writerows(csv_data)
